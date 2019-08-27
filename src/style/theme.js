bea1e2d9b6234122aa7cf39fcbe11b2df5de18b7
marcello luatti <marcello.luatti@e-globe-solutions.com>
2019-08-27 16:59:18 +0200

    [root] style directory added

---
+++ src/style/theme.js
@@ -0,0 +1,24 @@
+export default {
+  colors: {
+    blue: {
+      main: '#0d4d76',
+      active: '#086391',
+      disabled: '#086391',
+    },
+    green: {
+      main: '#88bd4f',
+      active: '#6e9e3b',
+      disabled: '#6e9e3b',
+    },
+    red: {
+      main: '#ff0000',
+      active: '#ff0000',
+      disabled: '#ff0000',
+    },
+    grey: {
+      dark: '#343434',
+      light: '#d8d8d8',
+      lightGrey: '#bdc9cc',
+    },
+  },
+}
